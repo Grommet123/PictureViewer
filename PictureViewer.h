ff1fa7b6c83e6b284fc0b2fc7257dd7a1f66cbb4
Gary Grotsky <gkgrotsky@aol.com>
2016-12-17 18:13:01 -0500

    v5.8 Display "of Number of files"

--- PictureViewer.h
+++ PictureViewer.h
@@ -34,7 +34,7 @@ Note: The picture file names must have the format picxx.bmp, where xx is a numbe
  ** DC -   Pin 9
  ** CS -   Pin 10
 */
-#define VERSION  "5.7"        // Version number
+#define VERSION  "5.8"        // Version number
 #define TFT_CS  10            // Chip select line for TFT display
 #define TFT_DC   9            // Data/command line for TFT
 #define TFT_RST  8            // Reset line for TFT
@@ -58,6 +58,7 @@ uint8_t read8(File f);
 uint16_t read16(File f);
 uint32_t read32(File f);
 void displaySplashScreen(displayModeEnum displayMode = NonTest);
-void displayFileName(char *fileName, bool fileFound = true, int bmpWidth = 0, int bmpHeight = 0);
+void displayFileName(char *fileName, bool fileFound = true, 
+                     int bmpWidth = 0, int bmpHeight = 0, int numberOfFiles = 99);
 
 #endif  // #define PictureViewer_h
